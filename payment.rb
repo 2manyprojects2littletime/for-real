94f450630460ed52db94b4bedcb93fad547c42ec
Matt Duffy <m.duffy12610@gmail.com>
2018-08-12 22:38:26 -0300

    move payment calculation into class

--- payment.rb
+++ payment.rb
@@ -1,5 +1,19 @@
-rate = 0.01
-principal = 100
-periods = 100
-compound_rate_factor = (1 + rate)**periods
-payment = principal*((rate*compound_rate_factor)/(compound_rate_factor - 1))
+# frozen_string_literal: true
+
+class Payment
+  attr_reader :payment
+  
+  def initialize(principal, rate, periods)
+    compound_rate_factor = compound_rate_factor(rate, periods)
+    @payment = principal * ((rate * compound_rate_factor) / (compound_rate_factor - 1))
+  end
+
+  def compound_rate_factor(rate, periods)
+    (1 + rate) ** periods
+  end
+
+  def to_s
+    @payment.round(2)
+  end
+  
+end
